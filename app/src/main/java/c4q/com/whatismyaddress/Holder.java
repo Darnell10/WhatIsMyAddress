0d44c601aa1d505112ed8e7e71d8fcabeaddf8cd
Darnell O <DarnellOtto@ac.c4q.nyc>
2017-11-21 22:00:21 -0500

    final commit

---
+++ app/src/main/java/c4q/com/whatismyaddress/Holder.java
@@ -0,0 +1,8 @@
+package c4q.com.whatismyaddress;
+
+/**
+ * Created by D on 11/21/17.
+ */
+
+public class Holder extends R {
+}
--- app/src/main/java/c4q/com/whatismyaddress/RecyclerActivity.java
+++ app/src/main/java/c4q/com/whatismyaddress/RecyclerActivity.java
@@ -1,13 +1,35 @@
 package c4q.com.whatismyaddress;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.lang.reflect.Array;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class RecyclerActivity extends AppCompatActivity {
+
+    private SharedPreferences getPref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recycler);
+
+        Intent intent = getIntent();
+        getPref = getApplication().getSharedPreferences(intent.getStringExtra("sharedPref"), MODE_PRIVATE);
+
+        HashMap<String, ?> enteries = new HashMap<>();
+
+        Map<String, ?> allEntries = getPref.getAll();
+        for (Map.Entry<String, ?> entry : allEntries.entrySet()) {
+            Log.d("map values", entry.getKey() + ": " + entry.getValue().toString());
+
+        }
     }
+
 }
