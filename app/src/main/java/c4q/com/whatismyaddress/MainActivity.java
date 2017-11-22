50954b896664e5b94b0364c47d6ab85f278ab95d
Darnell O <DarnellOtto@ac.c4q.nyc>
2017-11-21 20:47:37 -0500

    part01 commit

--- app/src/main/java/c4q/com/whatismyaddress/MainActivity.java
+++ app/src/main/java/c4q/com/whatismyaddress/MainActivity.java
@@ -1,13 +1,53 @@
 package c4q.com.whatismyaddress;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String SHARED_PREF_KEY ="address_shared_preferences";
+
+    private EditText email;
+    private Button submit;
+    private Button button_2;
+    private SharedPreferences save;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        email = (EditText) findViewById(R.id.email);
+        submit =(Button) findViewById(R.id.button1);
+        button_2 =(Button) findViewById(R.id.button2);
+        save = getApplicationContext().getSharedPreferences(SHARED_PREF_KEY,MODE_PRIVATE);
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences.Editor editor save.edit();
+                editor.putString("email", email.getText().toString());
+                editor.commit();
+
+            }
+        });
+
+        button_2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(MainActivity.this, RecyclerActivity.class);
+                intent.putExtra("sharedpref", SHARED_PREF_KEY);
+                startActivity(intent);
+
+            }
+        });
     }
 }
